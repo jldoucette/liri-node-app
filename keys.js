a685e62c0e1a0ce7aa82e4b95ca2c1291440b62f
JONATHAN DOUCETTE <Jonathan@JONATHANs-MacBook.local>
2017-05-23 12:52:31 -0700

    updated

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('This is loaded');
 exports.twitterKeys = {
-	consumer_key: 'pending',
-	consumer_secret: 'pending',
-	access_token_key: 'pending',
-	access_token_secret: 'pending',
+	consumer_key: 'EGyw4bYXWAKns46id1DYKtjbH',
+	consumer_secret: 'cRJFV2Zu54Wc1baPkytVyKVy2C8sp7KQSkmX33wEiWQILzySiA',
+	access_token_key: '	867086007216128000-6GQSUEBQXYBRjsblu8Yi2YuZu9O43QX',
+	access_token_secret: 'n8sdxY7FSVKg673M3HMcDnxoQplzfVJToQCPpLMChqSzb',
 }
--- liri.js
+++ liri.js
@@ -1,6 +1,52 @@
+var twitter=require('twitter');
+var spotify=require('spotify');
+var request=require('request');
+var inputRequest=process.argv[2];
 
 
-my-tweets
-spotify-this-song
-movie-this
-do-what-it-says
+switch (inputRequest) {
+case 'my-tweets':
+tweets();
+break;
+case 'spotify-this-song':
+spotifysong();
+break;
+case 'movie-this':
+movieinfo();
+break;
+case 'do-what-it-says':
+doit();
+break;
+}
+
+function tweets() {
+	console.log("Tweets");
+}
+
+function spotifysong() {
+	console.log("Spotify This Song");
+	spotify.search({ type: 'track', query: 'The Adventure' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+        else {
+        	console.log(JSON.stringify(data,null,2));
+        	var responseData=JSON.stringify(data);
+        	// var artistName=responseData.artists[0].name;
+        	// console.log(artistName);
+        }
+    
+ 
+    // Do something with 'data' 
+});
+}
+
+function movieinfo() {
+	console.log("Movie Info");
+
+}
+
+function doit() {
+	console.log("Do This Thing");
+}
