ea667597d92867fbce5a8ea6feb882a6815ef81b
jldoucette <jd@jonathandoucette.com>
2017-05-24 20:11:37 -0700

    Updated files

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('This is loaded');
 exports.twitterKeys = {
-	consumer_key: 'EGyw4bYXWAKns46id1DYKtjbH',
-	consumer_secret: 'cRJFV2Zu54Wc1baPkytVyKVy2C8sp7KQSkmX33wEiWQILzySiA',
-	access_token_key: '	867086007216128000-6GQSUEBQXYBRjsblu8Yi2YuZu9O43QX',
-	access_token_secret: 'n8sdxY7FSVKg673M3HMcDnxoQplzfVJToQCPpLMChqSzb',
+	consumer_key: 'uC7hiZ1nefwfJ3FmV5V7rq8y6',
+	consumer_secret: 'SPghrXBOAXksVNPGGCnkBhzakiUmg4IdUedVS6JcFxHexXOmR9',
+	access_token_key: '867086007216128000-6GQSUEBQXYBRjsblu8Yi2YuZu9O43QX',
+	access_token_secret: 'n8sdxY7FSVKg673M3HMcDnxoQplzfVJToQCPpLMChqSzb'
 }
--- liri.js
+++ liri.js
@@ -1,9 +1,16 @@
-var twitter=require('twitter');
+var Twitter=require('twitter');
 var spotify=require('spotify');
 var request=require('request');
+var keys = require("./keys.js");
+var fs=require('fs');
 var inputRequest=process.argv[2];
+var searchRequest=process.argv[3];
+var consumerKey=keys.twitterKeys.consumer_key;
+var consumerSecret=keys.twitterKeys.consumer_secret;
+var accessTokenKey=keys.twitterKeys.access_token_key;
+var accessTokenSecret=keys.twitterKeys.access_token_secret;
 
-
+function processInput() {
 switch (inputRequest) {
 case 'my-tweets':
 tweets();
@@ -18,27 +25,53 @@ case 'do-what-it-says':
 doit();
 break;
 }
-
+}
+processInput();
 function tweets() {
 	console.log("Tweets");
+var client = new Twitter({
+  consumer_key: consumerKey,
+  consumer_secret: consumerSecret,
+  access_token_key: accessTokenKey,
+  access_token_secret: accessTokenSecret
+});
+console.log("Client keys"+client);
+var params = {screen_name: 'jldoucette_bcs',
+count:20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for (i=0;i<tweets.length;i++){
+    console.log("Tweet: "+tweets[i].text+" Tweeted at: "+tweets[i].created_at);
+    }
+  }
+  else if (error) {
+    console.log(error);
+  }
+});
 }
 
 function spotifysong() {
 	console.log("Spotify This Song");
-	spotify.search({ type: 'track', query: 'The Adventure' }, function(err, data) {
+    if (searchRequest==null) {
+        searchRequest='"The Sign"';
+        console.log('SearchRequest: '+searchRequest);
+    }
+	spotify.search({ type: 'track', query: searchRequest }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
         else {
-        	console.log(JSON.stringify(data,null,2));
-        	var responseData=JSON.stringify(data);
-        	// var artistName=responseData.artists[0].name;
-        	// console.log(artistName);
+        	var artistName=data.tracks.items[0].album.artists[0].name;
+            var songName=data.tracks.items[0].album.name;
+            var albumName=data.tracks.items[0].name;
+            var previewLink=data.tracks.items[0].preview_url;
+
+            console.log("Artist: " +artistName);
+            console.log("Song Name: "+songName);
+            console.log("Preview Link: "+previewLink);
+        	console.log("Album Name: "+albumName);
         }
-    
- 
-    // Do something with 'data' 
 });
 }
 
@@ -49,4 +82,22 @@ function movieinfo() {
 
 function doit() {
 	console.log("Do This Thing");
+fs.readFile('random.txt', "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  else {
+    var randomArr=data.split(',');
+    console.log("randomArr "+randomArr);
+    inputRequest=randomArr[0];
+    searchRequest=randomArr[1];
+    console.log("New inputRequest: "+inputRequest);
+    console.log("New searchRequest: "+searchRequest);
+    console.log("Random.txt data: "+data);
+    processInput();
+
+}
+  
+
+});
 }
